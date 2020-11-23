b2c02cdf8c7a580fe591f5994d72081fd6b96016
priyanshusamal <61897869+priyanshusamal@users.noreply.github.com>
2020-11-23 18:38:01 +0530

    1

---
+++ Assets/Scripts/Player_View.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour
+{
+    public float mouse_sensivity =100f;
+    public Transform View;
+    float invertedY = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouse_sensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouse_sensivity * Time.deltaTime;
+        
+        invertedY -= mouseY;
+        invertedY = Mathf.Clamp(invertedY,-90f,90f);
+
+        transform.localRotation = Quaternion.Euler(invertedY,0f,0f);
+        View.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Scenemanager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
